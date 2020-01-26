4428df0855ffe7340affb4d9921d7f48393ff0b7
Oleg Stoianov <o.stoianov@campus.unimib.it>
2020-01-26 13:04:42 +0100

    Added CrossOrigin

--- Progetto-Aereo2/src/main/java/edu/progetto/security/AuthController.java
+++ Progetto-Aereo2/src/main/java/edu/progetto/security/AuthController.java
@@ -7,12 +7,14 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.progetto.cliente.Cliente;
 
+@CrossOrigin()
 @Controller
 public class AuthController {
 
